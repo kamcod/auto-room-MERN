70b7a6c1f3c7971efd9705887cdb37b0f855928a
kamcod <mkamran4786@gmail.com>
2023-02-18 01:14:43 +0500

    make user login/register apis and error handling

---
+++ server/routes/register.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const {SignUp, SignIn, Logout} = require('../controllers/register')
+
+router.route('/register').post(SignUp)
+router.route('/login').post(SignIn)
+router.route('/logout').delete(Logout)
+
+module.exports = router
