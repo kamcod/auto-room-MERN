b301451371072ba8bbaf2177673453e51ef87a31
kamcod <mkamran4786@gmail.com>
2023-07-14 06:21:42 +0500

    add upgrade plans ui

--- frontend/src/components/upgrade/Upgrade.jsx
+++ frontend/src/components/upgrade/Upgrade.jsx
@@ -1,7 +1,34 @@
+import PlanCard from './PlanCard';
+
+const PLANS = [
+  {
+    id: 0,
+    name: 'Basic',
+    price: 30,
+    currencyUnit: '$',
+    description: 'you will received basic plan badge'
+  },
+  {
+    id: 1,
+    name: 'Premium',
+    price: 50,
+    currencyUnit: '$',
+    description: 'you will received premium plan badge'
+  }
+];
+
 export default function Upgrade() {
   return (
-    <div>
-      Upgrade
+    <div className='product-page-wrapper'>
+      <h4 style={{textAlign: 'center', marginBottom: '20px'}}>Upgrade Plan</h4>
+      <div className='row d-flex justify-content-center'>
+      {PLANS?.map(plan => {
+        return <div className='col-3'>
+          <PlanCard plan = {plan} />
+        </div>
+      })}
+      </div>
+      
     </div>
   )
 }
